7661ab0217d92dabab02e354a5c1ebd86489d830
blackw00d <email>
2020-10-12 19:55:12 +0500

    bug fix

--- HFhtml/static/js/basket.js
+++ HFhtml/static/js/basket.js
@@ -1,12 +1,55 @@
-import './services'
+const fadeTime = 300;
 
 $(document).ready(function () {
-    recalculateCart(false);
+    recalculateCart();
 });
 
-/* Remove item from basket when click on .remove-product */
 $(document).delegate(".remove-product", "click", function (e) {
     e.preventDefault();
-    remove_item(this);
-    basket_remove();
+    removeItem(this);
+
+    const basketcounter = parseInt($('#basketbtn span').text());
+    const data = {};
+    const crsf_token = $('.shopping-cart [name="csrfmiddlewaretoken"]').val();
+    data['csrfmiddlewaretoken'] = crsf_token;
+    data['name'] = $(this).data('name');
+
+    const url = 'BasketRemove/';
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: data,
+        cache: true,
+        success: function () {
+            $('#basketbtn span').text((basketcounter - 1).toString());
+            recalculateCart();
+        }
+    })
 });
+
+function recalculateCart() {
+    let total = 0;
+
+    $('.product').each(function () {
+        total += parseFloat($(this).children('.product-price').text());
+    });
+
+    /* Update totals display */
+    $('.totals-value').fadeOut(fadeTime, function () {
+        $('#cart-total').html(total.toFixed(2) + " &#x20BD;");
+        if (total === 0) {
+            $('.checkout').fadeOut(fadeTime);
+        } else {
+            $('.checkout').fadeIn(fadeTime);
+        }
+        $('.totals-value').fadeIn(fadeTime);
+    });
+}
+
+/* Remove item from cart */
+function removeItem(removeButton) {
+    /* Remove row from DOM and recalculate cart total */
+    const productRow = $(removeButton).parent().parent();
+    productRow.remove();
+    recalculateCart();
+} 
--- HFhtml/static/js/checkout.js
+++ HFhtml/static/js/checkout.js
@@ -1,13 +1,57 @@
-import './services'
+const fadeTime = 300;
 
 $(document).ready(function () {
-    recalculateCart(true);
+    recalculateCart();
     $('input[name="selector"][value="online"]').prop('checked', true);
 });
 
-/* Remove item from basket when click on .remove-product */
 $(document).delegate(".remove-product", "click", function (e) {
     e.preventDefault();
-    remove_item(this);
-    basket_remove();
+    removeItem(this);
+
+    const basketcounter = parseInt($('#basketbtn span').text());
+    const data = {};
+    data['csrfmiddlewaretoken'] = $('.shopping-cart [name="csrfmiddlewaretoken"]').val();
+    data['name'] = $(this).data('name');
+
+    const url = '../BasketRemove/';
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: data,
+        cache: true,
+        success: function () {
+            $('#basketbtn span').text((basketcounter - 1).toString());
+            recalculateCart();
+        }
+    })
 });
+
+function recalculateCart() {
+    let total = 0;
+
+    $('.product').each(function () {
+        total += parseFloat($(this).children('.product-price').text());
+    });
+
+    /* Update totals display */
+    $('.totals-value').fadeOut(fadeTime, function () {
+        $('#cart-total').html(total.toFixed(2) + " &#x20BD;");
+        if (total === 0) {
+            $('.checkout').fadeOut(fadeTime);
+            $('.shopping-cart').html("<h1>Ваша корзина пуста</h1>\n" +
+                "<h0>Корзина ждет, что ее наполнят. Желаем приятных покупок!</h0>");
+        } else {
+            $('.checkout').fadeIn(fadeTime);
+        }
+        $('.totals-value').fadeIn(fadeTime);
+    });
+}
+
+/* Remove item from cart */
+function removeItem(removeButton) {
+    /* Remove row from DOM and recalculate cart total */
+    const productRow = $(removeButton).parent().parent();
+    productRow.remove();
+    recalculateCart();
+}
